f4f56e1ea6dad86dc8961c4776892b013e147ac3
avicndugu <37213663+avicndugu@users.noreply.github.com>
2025-02-08 11:09:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: 'index.html',
-                '2': '/2/index.html',
-                '3': '/3/index.html',
-                '4': '/4/index.html',
             },
         },
     },
